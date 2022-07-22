3d9f8e96639ba7da0ad1bcb6342931488163b294
reduan2660 <alve.reduan.09@gmail.com>
2022-07-22 18:41:37 +0600

    OTP Verification

--- src/main/java/com/lucent/backend/Notifications/TextService.java
+++ src/main/java/com/lucent/backend/Notifications/TextService.java
@@ -1,13 +1,35 @@
 package com.lucent.backend.Notifications;
 
 import com.lucent.backend.domain.AppUser;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class TextService {
-    public void sendVerficationText(AppUser user){
-        System.out.println(user.getPhone() + " " + user.getVerificationCode());
+
+    @Value("${smsapiusername}")
+    private String apiUsername;
+    @Value("${smsapipassword}")
+    private String apiPassword;
+    public void sendVerficationText(AppUser user)  {
+        String message = String.valueOf(user.getVerificationCode()) + "%20is%20your%20OTP%20verification%20code%20for%20Lucent.";
+        String url = "http://66.45.237.70/api.php?" + "username=" + apiUsername + "&password=" + apiPassword + "&number=" + user.getPhone() + "&message=" + message;
+
+        try {
+            Unirest.setTimeouts(0, 0);
+            System.out.println("url = " + url);
+            HttpResponse<String> response = Unirest.post(url)
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .body("")
+                    .asString();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
     }
 }
